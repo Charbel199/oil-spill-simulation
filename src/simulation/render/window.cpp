60a3149f5081563caabcce7e11e84a8d11c7e504
Charbel199 <charbel-boumaroun@outlook.com>
2022-06-21 16:38:06 +0200

    Change Window class to namespace

--- src/simulation/render/window.cpp
+++ src/simulation/render/window.cpp
@@ -1,44 +1,63 @@
 #include "window.h"
 #include <GL/glut.h>
+#include <GL/freeglut.h>
 #include <cmath>
+#include <cstring>
+#include "open_gl_helper.h"
+namespace Window{
+    int timeElapsedForSetup;
+
+    void drawFps() {
+        Color color= {1, 0 ,0 ,0};
+        Position pos = {0, 640,480 ,0};
+        PositionXY posXY = {350, 460};
+        OpenGLHelper::drawText(color, pos, posXY, "TEST");
+    }
+
+    void draw()
+    {
+        drawFps();
+        std::cout<<"DRAWING VERTEX"<<std::endl;
 
 
-void display()
-{
-    glClearColor(0, 0, 0, 1);
-    glClear(GL_COLOR_BUFFER_BIT);
+        glBegin(GL_QUADS);
+        glColor3f(1.0, 0, 0);
+        glVertex3f(0.5, 0.5, 1);
+        glColor3f(0, 1.0, 0);
+        glVertex3f(0.5, 0, 1);
+        glColor3f(0, 0, 1.0);
+        glVertex3f(0, 0.5, 1);
+        glVertex3f(0.5, 0.5, 1);
 
-    glBegin(GL_QUADS);
-    glColor3f(1.0, 0, 0);
-    glVertex3f(0.5, 0.5, 1);
-    glColor3f(0, 1.0, 0);
-    glVertex3f(0.5, 0, 1);
-    glColor3f(0, 0, 1.0);
-    glVertex3f(0, 0.5, 1);
-    glVertex3f(0.5, 0.5, 1);
+        glEnd();
 
-    glEnd();
+        glFlush();
 
-    glFlush();
-}
-Window::Window(int argc, char** argv, std::string title, float translationSpeed) {
-    // Initialize OpenGL
-    glutInit(&argc, argv);
-    glutInitWindowSize(WIDTH, HEIGHT);
-    glutInitWindowPosition(10, 10);
-    glutCreateWindow(title.c_str());
-    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH | GLUT_MULTISAMPLE);
-    glEnable(GL_MULTISAMPLE);
+    }
+    void initializeWindow(int argc, char** argv, std::string title, float translationSpeed) {
+        // Initialize OpenGL
+        glutInit(&argc, argv);
+        glutInitWindowSize(WIDTH, HEIGHT);
+        glutInitWindowPosition(10, 10);
+        glutCreateWindow(title.c_str());
+        glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH | GLUT_MULTISAMPLE);
+        glEnable(GL_MULTISAMPLE);
+
+    }
 
 
+    void run() {
+        timeElapsedForSetup = glutGet(GLUT_ELAPSED_TIME);
 
-}
+        // Set drawing function (It will loop while the simulation is running)
+        glutDisplayFunc(draw);
 
-void Window::run() {
-    timeElapsedForSetup = glutGet(GLUT_ELAPSED_TIME);
+        // Set background color
+        glClearColor(1 ,1 ,1 , 1);
+        glClear ( GL_COLOR_BUFFER_BIT ) ;
 
+        // Run main loop
+        glutMainLoop();
+    }
 
-    // Run simulation
-    glutDisplayFunc(display);
-    glutMainLoop();
 }
--- src/simulation/render/window.h
+++ src/simulation/render/window.h
@@ -6,21 +6,24 @@
 
 #include "iostream"
 
-class Window{
-    public:
-        Window(int argc, char** argv, std::string title, float translationSpeed);
+namespace Window{
 
-        void draw();
-        void keyDown(unsigned char, int, int);
-        void keyUp(unsigned char, int, int);
-        void mouseMove(int, int);
-        void reshape(int, int);
 
-        void run();
-        void quit();
 
-        int timeElapsedForSetup;
 
+    void initializeWindow(int argc, char** argv, std::string title, float translationSpeed);
+    void draw();
+    void keyDown(unsigned char, int, int);
+    void keyUp(unsigned char, int, int);
+    void mouseMove(int, int);
+    void reshape(int, int);
+
+    void run();
+    void quit();
+
+
+
+    void drawFps();
 };
 
 
